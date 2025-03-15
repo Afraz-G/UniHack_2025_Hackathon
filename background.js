327ef6d2d1e099659ca39283547a9680614d9b2d
GcNoteX <gcha0054@student.monash.edu>
2025-03-15 14:16:29 +1100

    Monitor List Functionality
    - Reads URL to determine if it is a bad website
    - Simple saving and deleting from chrome sync storage of list

---
+++ background.js
@@ -0,0 +1,9 @@
+// THIS IS SYNTAX FOR LISTENING TO  THE WEBSITES TAB
+
+// chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+//     // if (tab.url && tab.url.includes("youtube.com/watch")){ // This things triggers multiple times on a single chrome update
+//     //     console.log("Youtube openeed!");
+//     //     console.log("Youtube tab!");
+//     // }
+
+// })
